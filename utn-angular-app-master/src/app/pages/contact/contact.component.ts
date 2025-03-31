ca3b4ddabde7eddd3cb66ea605f45b1387292d6c
santid.net@gmail.com <santid.net@gmail.com>
2025-03-31 09:48:42 -0300

    c

---
+++ utn-angular-app-master/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contact.component.html'
+})
+export class ContactoComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  enviar() {
+    if (this.form.valid) {
+      console.log('Formulario enviado:', this.form.value);
+      alert('Mensaje enviado con éxito!');
+      this.form.reset();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
